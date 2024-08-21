eda6dd4ba3ee686126d85f8977056394f21883d1
perfectbullet <121531845@qq.com>
2024-08-21 17:19:01 +0800

    update

--- start_work.sh
+++ start_work.sh
@@ -6,3 +6,8 @@ export CUDA_VISIBLE_DEVICES=1
 # llamafactory-cli webui
 # nohup jupyter-lab --ip=0.0.0.0 --port=8889 --no-browser --allow-root &
 nohup jupyter lab --ip=0.0.0.0 --port=8889 --no-browser --allow-root > log-jupyterlab.log 2>&1 &
+
+export MONGODB_URL=mongodb://admin:123456@10.0.1.40/?authSource=admin
+nohup python src/api.py > api-log.log 2>&1 &
+
+
