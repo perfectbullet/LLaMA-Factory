093bdcfb892869c10bba8277668d7e7e0a402862
zj <zhoujing382@gmail.com>
2024-09-19 19:34:00 +0800

    增加数据

--- src/webui.py
+++ src/webui.py
@@ -9,7 +9,8 @@ matplotlib.use('TkAgg')
 def main():
     gradio_share = os.environ.get("GRADIO_SHARE", "0").lower() in ["true", "1"]
     server_name = os.environ.get("GRADIO_SERVER_NAME", "0.0.0.0")
-    server_port = os.environ.get("GRADIO_SERVER_PORT", 7871)
+    server_port = int(os.environ.get("GRADIO_SERVER_PORT", 7871))
+
     create_ui().queue().launch(
         share=gradio_share,
         server_name=server_name,
